1e4f8f579f192ea840face636a2bf578a7da378a
Srikar2020 <68241974+Srikar2020@users.noreply.github.com>
2020-12-22 18:15:18 -0800

    Added and tested

--- app.js
+++ app.js
@@ -3,29 +3,29 @@ const mysql = require('mysql2');
 const cTable = require('console.table');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  // Your MySQL username
-  user: 'root',
-  // Your MySQL password
-  password: 'password',
-  database: 'Employee_Tracker',
-  //Promise: bluebird
+    host: 'localhost',
+    port: 3306,
+    // Your MySQL username
+    user: 'root',
+    // Your MySQL password
+    password: 'password',
+    database: 'Employee_Tracker',
+    //Promise: bluebird
 });
 
 connection.connect(err => {
     if (err) throw err;
     console.log('connected as id ' + connection.threadId + '\n');
-    
+
     promptMenu();
 });
 
 // gets all departments names in an array
 const getDepartments = () => {
     return new Promise(resolve => {
-        connection.query('SELECT name FROM department', function(err, res) {
+        connection.query('SELECT name FROM department', function (err, res) {
             if (err) throw err;
-                // Log all results of the SELECT statement
+            // Log all results of the SELECT statement
             resolve(res.map(data => data.name));
         });
     });
@@ -34,9 +34,9 @@ const getDepartments = () => {
 // gets all role names in an array
 const getRoles = () => {
     return new Promise(resolve => {
-        connection.query('SELECT title FROM role', function(err, res) {
+        connection.query('SELECT title FROM role', function (err, res) {
             if (err) throw err;
-                // Log all results of the SELECT statement
+            // Log all results of the SELECT statement
             resolve(res.map(data => data.title));
         });
     });
@@ -45,10 +45,10 @@ const getRoles = () => {
 // gets all employee names in an array
 const getEmployees = () => {
     return new Promise(resolve => {
-        connection.query('SELECT * FROM employee', function(err, res) {
+        connection.query('SELECT * FROM employee', function (err, res) {
             if (err) throw err;
-                // Log all results of the SELECT statement
-            resolve(res.map(data => data.first_name.concat(" ",data.last_name)));
+            // Log all results of the SELECT statement
+            resolve(res.map(data => data.first_name.concat(" ", data.last_name)));
         });
     });
 }
@@ -56,43 +56,44 @@ const getEmployees = () => {
 // gets the department id given the name
 const getDepartmentID = (department) => {
     return new Promise(resolve => {
-        connection.query('SELECT id FROM department WHERE ?', 
-        {
-            name: department
-        },
-        function(err, res) {
-            if (err) throw err;
+        connection.query('SELECT id FROM department WHERE ?',
+            {
+                name: department
+            },
+            function (err, res) {
+                if (err) throw err;
                 // Log all results of the SELECT statement
-            resolve(res[0].id);
-        });
+                resolve(res[0].id);
+            });
     });
 }
 
 // gets the role id given the name
 const getRoleID = (role) => {
     return new Promise(resolve => {
-        connection.query('SELECT id FROM role WHERE ?', 
-        {
-            title: role
-        },
-        function(err, res) {
-            if (err) throw err;
+        connection.query('SELECT id FROM role WHERE ?',
+            {
+                title: role
+            },
+            function (err, res) {
+                if (err) throw err;
                 // Log all results of the SELECT statement
-            resolve(res[0].id);
-        });
+                resolve(res[0].id);
+            });
     });
 }
 
 // gets the employee id given the name
 const getEmployeeID = (employee) => {
+    console.log(employee.split(" "));
     return new Promise(resolve => {
-        connection.query('SELECT id FROM employee WHERE `first_name` = ? AND `last_name` = ?', 
-        employee.split(" "),
-        function(err, res) {
-            if (err) throw err;
+        connection.query('SELECT id FROM employee WHERE `first_name` = ? AND `last_name` = ?',
+            employee.split(" "),
+            function (err, res) {
+                if (err) throw err;
                 // Log all results of the SELECT statement
-            resolve(res[0].id);
-        });
+                resolve(res[0].id);
+            });
     });
 }
 
@@ -106,10 +107,10 @@ const addDepartment = () => {
                 name: 'name',
                 message: 'Enter the department name:',
                 validate: nameInput => {
-                    if(nameInput) {
+                    if (nameInput) {
                         return true;
                     }
-                    else{
+                    else {
                         console.log("Please enter the department name!");
                         return false;
                     }
@@ -119,16 +120,16 @@ const addDepartment = () => {
             const query = connection.query(
                 'INSERT INTO department SET ?',
                 {
-                  name: data.name
+                    name: data.name
                 },
-                function(err, res) {
-                  if (err) throw err;
-                  console.log(res.affectedRows + ' department inserted!\n');
+                function (err, res) {
+                    if (err) throw err;
+                    console.log(res.affectedRows + ' department inserted!\n');
 
-                  resolve({
-                    ok: true,
-                    message: 'Success!'
-                   });
+                    resolve({
+                        ok: true,
+                        message: 'Success!'
+                    });
                 }
             );
         });
@@ -146,10 +147,10 @@ const addRole = () => {
                     name: 'name',
                     message: 'Enter the role name:',
                     validate: nameInput => {
-                        if(nameInput) {
+                        if (nameInput) {
                             return true;
                         }
-                        else{
+                        else {
                             console.log("Please enter the role name!");
                             return false;
                         }
@@ -160,10 +161,10 @@ const addRole = () => {
                     name: 'salary',
                     message: 'Enter the role salary:',
                     validate: salaryInput => {
-                        if(salaryInput) {
+                        if (salaryInput) {
                             return true;
                         }
-                        else{
+                        else {
                             console.log("Please enter the role salary!");
                             return false;
                         }
@@ -184,14 +185,14 @@ const addRole = () => {
                             salary: data.salary,
                             department_id: id
                         },
-                        function(err, res) {
-                          if (err) throw err;
-                          console.log(res.affectedRows + ' role inserted!\n');
-                          
-                          resolve({
-                            ok: true,
-                            message: 'Success!'
-                           });
+                        function (err, res) {
+                            if (err) throw err;
+                            console.log(res.affectedRows + ' role inserted!\n');
+
+                            resolve({
+                                ok: true,
+                                message: 'Success!'
+                            });
                         }
                     );
                 })
@@ -212,10 +213,10 @@ const addEmployee = () => {
                         name: 'first_name',
                         message: "Enter the employee's first name:",
                         validate: nameInput => {
-                            if(nameInput) {
+                            if (nameInput) {
                                 return true;
                             }
-                            else{
+                            else {
                                 console.log("Please enter the employee's first name");
                                 return false;
                             }
@@ -226,10 +227,10 @@ const addEmployee = () => {
                         name: 'last_name',
                         message: "Enter the employee's last name:",
                         validate: nameInput => {
-                            if(nameInput) {
+                            if (nameInput) {
                                 return true;
                             }
-                            else{
+                            else {
                                 console.log("Please enter the employee's last name");
                                 return false;
                             }
@@ -249,7 +250,7 @@ const addEmployee = () => {
                     }
                 ]).then(data => {
                     getRoleID(data.role).then(role_id => {
-                        if(data.manager === 'None'){
+                        if (data.manager === 'None') {
                             const query = connection.query(
                                 'INSERT INTO employee SET ?',
                                 {
@@ -258,13 +259,13 @@ const addEmployee = () => {
                                     role_id: role_id,
                                     manager_id: null
                                 },
-                                function(err, res) {
-                                  if (err) throw err;
-                                  console.log(res.affectedRows + ' employee inserted!\n');
+                                function (err, res) {
+                                    if (err) throw err;
+                                    console.log(res.affectedRows + ' employee inserted!\n');
                                 }
                             );
                         }
-                        else{
+                        else {
                             getEmployeeID(data.manager).then(manager_id => {
                                 const query = connection.query(
                                     'INSERT INTO employee SET ?',
@@ -274,9 +275,9 @@ const addEmployee = () => {
                                         role_id: role_id,
                                         manager_id: manager_id
                                     },
-                                    function(err, res) {
-                                      if (err) throw err;
-                                      console.log(res.affectedRows + ' employee inserted!\n');
+                                    function (err, res) {
+                                        if (err) throw err;
+                                        console.log(res.affectedRows + ' employee inserted!\n');
                                     }
                                 );
                             })
@@ -313,24 +314,24 @@ const updateRole = () => {
                 ]).then(data => {
                     getRoleID(data.role).then(role_id => {
                         getEmployeeID(data.employee).then(employee_id => {
-                            const query = connection.query(
+                            connection.query(
                                 'UPDATE employee SET ? WHERE ?',
                                 [
                                     {
-                                     role_id: role_id
+                                        role_id: role_id
                                     },
                                     {
                                         id: employee_id
                                     }
                                 ],
-                                function(err, res) {
-                                  if (err) throw err;
-                                  console.log(res.affectedRows + ' employee updated!\n');
+                                function (err, res) {
+                                    if (err) throw err;
+                                    console.log(res.affectedRows + ' employee updated!\n');
 
-                                  resolve({
-                                    ok: true,
-                                    message: 'Success!'
-                                   });
+                                    resolve({
+                                        ok: true,
+                                        message: 'Success!'
+                                    });
                                 }
                             );
                         })
@@ -344,7 +345,7 @@ const updateRole = () => {
 // print department table to console
 const viewDepartments = () => {
     return new Promise(resolve => {
-        connection.query('SELECT * FROM department', function(err, res) {
+        connection.query('SELECT * FROM department', function (err, res) {
             if (err) throw err;
             // Log all results of the SELECT statement
             console.table(res);
@@ -362,7 +363,7 @@ const viewRoles = () => {
         const sql = `SELECT role.id, role.title, role.salary, department.name AS department 
         FROM role
         LEFT JOIN department ON role.department_id = department.id`;
-        connection.query(sql, function(err, res) {
+        connection.query(sql, function (err, res) {
             if (err) throw err;
             // Log all results of the SELECT statement
             console.table(res);
@@ -384,7 +385,7 @@ const viewEmployees = () => {
         LEFT JOIN role ON e.role_id = role.id
         LEFT JOIN department ON role.department_id = department.id
         LEFT JOIN employee m on m.id = e.manager_id`;
-        connection.query(sql, function(err, res) {
+        connection.query(sql, function (err, res) {
             if (err) throw err;
             // Log all results of the SELECT statement
             console.table(res);
@@ -398,53 +399,53 @@ const viewEmployees = () => {
 }
 
 // prompt the menu
-const promptMenu = ()  => {
+const promptMenu = () => {
     return inquirer.prompt([
         {
-          type: 'list',
-          name: 'option',
-          message: 'What would you like to do?',
-          choices: ['View All Departments','View All Roles', 'View All Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role', 'Exit']
+            type: 'list',
+            name: 'option',
+            message: 'What would you like to do?',
+            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role', 'Exit']
         }
     ])
-    .then(menuData => {
-        if(menuData.option === 'View All Departments'){
-            viewDepartments().then(message => {
-                promptMenu();
-            })
-        }
-        else if(menuData.option === 'View All Roles'){
-            viewRoles().then(message => {
-                promptMenu();
-            })
-        }
-        else if(menuData.option === 'View All Employees'){
-            viewEmployees().then(message => {
-                promptMenu();
-            })
-        }
-        else if(menuData.option === 'Add Department'){
-            addDepartment().then(message => {
-                promptMenu();
-            })
-        }
-        else if(menuData.option === 'Add Role'){
-            addRole().then(message => {
-                promptMenu();
-            })
-        }
-        else if(menuData.option === 'Add Employee'){
-            addEmployee().then(message => {
-                promptMenu();
-            })
-        }
-        else if(menuData.option === 'Update Employee Role'){
-            updateRole().then(message => {
-                promptMenu();
-            })
-        }
-        else{
-            connection.end();
-        }
-    })
+        .then(menuData => {
+            if (menuData.option === 'View All Departments') {
+                viewDepartments().then(message => {
+                    promptMenu();
+                })
+            }
+            else if (menuData.option === 'View All Roles') {
+                viewRoles().then(message => {
+                    promptMenu();
+                })
+            }
+            else if (menuData.option === 'View All Employees') {
+                viewEmployees().then(message => {
+                    promptMenu();
+                })
+            }
+            else if (menuData.option === 'Add Department') {
+                addDepartment().then(message => {
+                    promptMenu();
+                })
+            }
+            else if (menuData.option === 'Add Role') {
+                addRole().then(message => {
+                    promptMenu();
+                })
+            }
+            else if (menuData.option === 'Add Employee') {
+                addEmployee().then(message => {
+                    promptMenu();
+                })
+            }
+            else if (menuData.option === 'Update Employee Role') {
+                updateRole().then(message => {
+                    promptMenu();
+                })
+            }
+            else {
+                connection.end();
+            }
+        })
 }
